adf5c8f6feb38e9a958bf3185ac67a300aee2392
Caleb Akomaye <calebakomaye@outlook.com>
2024-05-07 21:21:13 +0100

    fixed another typo in heading.tsx

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -13,7 +13,7 @@ export const Heading = () => {
   return (
     <div className='max-w-3xl space-y-4'>
       <h1 className='text-3xl sm:text-5xl md:text-6xl font-bold'>
-        Write, Plan, Oragnise, Play
+        Write, Plan, Organise, Play
       </h1>
       <h3 className='text-base sm:text-xl md:text-2xl font-medium'>
         Turn Ideas into Action with Notely
